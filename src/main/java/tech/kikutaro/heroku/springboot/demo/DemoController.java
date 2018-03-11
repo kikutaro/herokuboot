4fbd40f98245d5a8afb1e5a21b0a2646c9fc2a18
kikutaro <kikutaro526@yahoo.co.jp>
2018-03-11 23:09:46 +0900

    Add CrossOrigin Annotation for JavaScript sample.

--- src/main/java/tech/kikutaro/heroku/springboot/demo/DemoController.java
+++ src/main/java/tech/kikutaro/heroku/springboot/demo/DemoController.java
@@ -1,11 +1,13 @@
 package tech.kikutaro.heroku.springboot.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     
+    @CrossOrigin
     @GetMapping("/hello")
     public String hello() {
         return "hello";
